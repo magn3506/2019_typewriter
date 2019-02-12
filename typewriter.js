86f989a4be5c2a6df4a37e0b01173639726e9329
Magnus Vagn Jensen <magnusvagnjensen@laptop-5v69gkts.kea.local>
2019-02-12 08:31:52 +0100

    hej

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,29 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", typewriter);
+
+let text = document.querySelector("#typewriter").innerHTML;
+
+let i = 0;
+
+let speed = 100;
+
+document.querySelector("#typewriter").textContent = " ";
+
+function typewriter() {
+  if (i == text.length) {
+    i = 0;
+
+    setTimeout(reset, 1000);
+  } else if (i < text.length) {
+    document.querySelector("#typewriter").innerHTML += text.charAt(i);
+    i++;
+
+    setTimeout(typewriter, speed);
+  }
+}
+
+function reset() {
+  document.querySelector("#typewriter").textContent = " ";
+  typewriter();
+}
